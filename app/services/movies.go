b53624c7290c33132814086edd507fb7a085afce
Bastien <neitsabast@gmail.com>
2023-11-07 14:53:51 +0100

    first version

--- app/services/movies.go
+++ app/services/movies.go
@@ -1,14 +1,30 @@
 package services
 
 import (
+	"encoding/json"
+	"log"
 	rediscon "supVOD/app/connectors/redisConf"
 	"supVOD/app/functions"
 	"supVOD/app/models"
 )
 
-func CreateMovie(m models.Movie) {
+func CreateMovie(m models.Movie) error {
+	var (
+		err error
+		val []byte
+	)
+
 	redisInstance := rediscon.GetRedisInstance()
 
 	m.ID = functions.NewUUID()
 	key := m.TableName() + "/" + m.ID
+
+	log.Println(m.ID)
+
+	if val, err = json.Marshal(m); err == nil {
+		err = redisInstance.Set(key, string(val), 0).Err()
+	}
+
+	log.Println(err)
+	return err
 }
